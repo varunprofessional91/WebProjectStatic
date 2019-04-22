66b8619be1dbce3b3f33680fb37553887015ff17
Varun Kumar <varunprofessional91@gmail.com>
2019-04-22 15:11:15 +0530

    Add files via upload

---
+++ Web Assignment/scripts/myscript.js
@@ -0,0 +1,16 @@
+/*
+
+Copyright Reserved By Varun Kumar
+
+*/
+
+
+function copyright_year()
+        {
+
+         var today_date = new Date();
+         var current_year = today_date.getFullYear();
+            document.write(current_year);
+
+        }
+
